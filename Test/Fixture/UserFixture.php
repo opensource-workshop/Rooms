e6d0227fdee6c22f66fb8ff67dc4637178e3c800
s-nakajima <nakajimashouhei@gmail.com>
2014-10-18 16:34:48 +0900

    fix build error

--- Test/Fixture/UserFixture.php
+++ Test/Fixture/UserFixture.php
@@ -2,13 +2,18 @@
 /**
  * UserFixture
  *
- * @author   Jun Nishikawa <topaz2@m0n0m0n0.com>
- * @link     http://www.netcommons.org NetCommons Project
- * @license  http://www.netcommons.org/license.txt NetCommons License
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
  */
 
 /**
- * Summary for UserFixture
+ * UserFixture
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Rooms\Test\Fixture
  */
 class UserFixture extends CakeTestFixture {
 
