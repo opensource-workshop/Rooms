f71880179e969da3d19dc9c3d06609833185897f
s-nakajima <nakajimashouhei@gmail.com>
2015-09-16 15:02:32 +0900

    テストの共通化

--- Test/Fixture/RoomFixture.php
+++ Test/Fixture/RoomFixture.php
@@ -9,13 +9,15 @@
  * @copyright Copyright 2014, NetCommons Project
  */
 
+App::uses('NetCommonsCakeTestFixture', 'NetCommons.TestSuite/Fixture');
+
 /**
  * RoomFixture
  *
  * @author Shohei Nakajima <nakajimashouhei@gmail.com>
  * @package NetCommons\Rooms\Test\Fixture
  */
-class RoomFixture extends CakeTestFixture {
+class RoomFixture extends NetCommonsCakeTestFixture {
 
 /**
  * Fields
@@ -31,6 +33,7 @@ class RoomFixture extends CakeTestFixture {
 		'lft' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'rght' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'active' => array('type' => 'boolean', 'null' => true, 'default' => null),
+		'default_role_key' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8', 'comment' => '「ルーム内の役割」のデフォルト値'),
 		'need_approval' => array('type' => 'boolean', 'null' => true, 'default' => null),
 		'default_participation' => array('type' => 'boolean', 'null' => true, 'default' => null),
 		'page_layout_permitted' => array('type' => 'boolean', 'null' => true, 'default' => null),
@@ -55,10 +58,12 @@ class RoomFixture extends CakeTestFixture {
 			'id' => '1',
 			'space_id' => '2',
 			'page_id_top' => '1',
+			'root_id' => null,
 			'parent_id' => null,
 			'lft' => '1',
 			'rght' => '2',
 			'active' => '1',
+			'default_role_key' => 'visitor',
 			'need_approval' => '1',
 			'default_participation' => '1',
 			'page_layout_permitted' => '1',
@@ -68,10 +73,12 @@ class RoomFixture extends CakeTestFixture {
 			'id' => '2',
 			'space_id' => '3',
 			'page_id_top' => null,
+			'root_id' => null,
 			'parent_id' => null,
 			'lft' => '3',
 			'rght' => '4',
 			'active' => '1',
+			'default_role_key' => 'room_administrator',
 			'need_approval' => '0',
 			'default_participation' => '0',
 			'page_layout_permitted' => '0',
@@ -81,10 +88,12 @@ class RoomFixture extends CakeTestFixture {
 			'id' => '3',
 			'space_id' => '4',
 			'page_id_top' => null,
+			'root_id' => null,
 			'parent_id' => null,
 			'lft' => '5',
 			'rght' => '6',
 			'active' => '1',
+			'default_role_key' => 'general_user',
 			'need_approval' => '1',
 			'default_participation' => '1',
 			'page_layout_permitted' => '1',
