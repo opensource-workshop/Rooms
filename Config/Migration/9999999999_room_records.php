73d0c9942abda5ebc665734e38352a64afcb1ca4
s-nakajima <nakajimashouhei@gmail.com>
2015-11-13 16:19:07 +0900

    カレンダーの予定追加権限処理削除、メール通知権限追加

--- Config/Migration/9999999999_room_records.php
+++ Config/Migration/9999999999_room_records.php
@@ -48,12 +48,12 @@ class RoomRecords extends NetCommonsMigration {
 			array('role_key' => 'editor', 'type' => 'room_role', 'permission' => 'html_not_limited', 'value' => '0', 'fixed' => '0', ),
 			array('role_key' => 'general_user', 'type' => 'room_role', 'permission' => 'html_not_limited', 'value' => '0', 'fixed' => '0', ),
 			array('role_key' => 'visitor', 'type' => 'room_role', 'permission' => 'html_not_limited', 'value' => '0', 'fixed' => '1', ),
-			//カレンダーの投稿制限
-			array('role_key' => 'room_administrator', 'type' => 'room_role', 'permission' => 'calendar_creatable', 'value' => '1', 'fixed' => '1', ),
-			array('role_key' => 'chief_editor', 'type' => 'room_role', 'permission' => 'calendar_creatable', 'value' => '1', 'fixed' => '1', ),
-			array('role_key' => 'editor', 'type' => 'room_role', 'permission' => 'calendar_creatable', 'value' => '1', 'fixed' => '1', ),
-			array('role_key' => 'general_user', 'type' => 'room_role', 'permission' => 'calendar_creatable', 'value' => '1', 'fixed' => '0', ),
-			array('role_key' => 'visitor', 'type' => 'room_role', 'permission' => 'calendar_creatable', 'value' => '0', 'fixed' => '1', ),
+			//投稿をメール通知する権限
+			array('role_key' => 'room_administrator', 'type' => 'room_role', 'permission' => 'mail_content_receivable ', 'value' => '1', 'fixed' => '1', ),
+			array('role_key' => 'chief_editor', 'type' => 'room_role', 'permission' => 'mail_content_receivable ', 'value' => '1', 'fixed' => '1', ),
+			array('role_key' => 'editor', 'type' => 'room_role', 'permission' => 'mail_content_receivable ', 'value' => '1', 'fixed' => '0', ),
+			array('role_key' => 'general_user', 'type' => 'room_role', 'permission' => 'mail_content_receivable ', 'value' => '1', 'fixed' => '0', ),
+			array('role_key' => 'visitor', 'type' => 'room_role', 'permission' => 'mail_content_receivable ', 'value' => '0', 'fixed' => '0', ),
 		),
 		'RolesRoom' => array(
 			//パブリックスペース
@@ -92,7 +92,7 @@ class RoomRecords extends NetCommonsMigration {
 			array('roles_room_id' => '1', 'permission' => 'content_readable', 'value' => '1'),
 			array('roles_room_id' => '1', 'permission' => 'page_editable', 'value' => '1'),
 			array('roles_room_id' => '1', 'permission' => 'html_not_limited', 'value' => '1'),
-			array('roles_room_id' => '1', 'permission' => 'calendar_creatable', 'value' => '1'),
+			array('roles_room_id' => '1', 'permission' => 'mail_content_receivable ', 'value' => '1'),
 			//--編集長
 			array('roles_room_id' => '2', 'permission' => 'block_editable', 'value' => '1'),
 			array('roles_room_id' => '2', 'permission' => 'content_comment_creatable', 'value' => '1'),
@@ -104,7 +104,7 @@ class RoomRecords extends NetCommonsMigration {
 			array('roles_room_id' => '2', 'permission' => 'content_readable', 'value' => '1'),
 			array('roles_room_id' => '2', 'permission' => 'page_editable', 'value' => '1'),
 			array('roles_room_id' => '2', 'permission' => 'html_not_limited', 'value' => '0'),
-			array('roles_room_id' => '2', 'permission' => 'calendar_creatable', 'value' => '1'),
+			array('roles_room_id' => '2', 'permission' => 'mail_content_receivable ', 'value' => '1'),
 			//--編集者
 			array('roles_room_id' => '3', 'permission' => 'block_editable', 'value' => '0'),
 			array('roles_room_id' => '3', 'permission' => 'content_comment_creatable', 'value' => '1'),
@@ -116,7 +116,7 @@ class RoomRecords extends NetCommonsMigration {
 			array('roles_room_id' => '3', 'permission' => 'content_readable', 'value' => '1'),
 			array('roles_room_id' => '3', 'permission' => 'page_editable', 'value' => '0'),
 			array('roles_room_id' => '3', 'permission' => 'html_not_limited', 'value' => '0'),
-			array('roles_room_id' => '3', 'permission' => 'calendar_creatable', 'value' => '1'),
+			array('roles_room_id' => '3', 'permission' => 'mail_content_receivable ', 'value' => '1'),
 			//--一般
 			array('roles_room_id' => '4', 'permission' => 'block_editable', 'value' => '0'),
 			array('roles_room_id' => '4', 'permission' => 'content_comment_creatable', 'value' => '0'),
@@ -128,7 +128,7 @@ class RoomRecords extends NetCommonsMigration {
 			array('roles_room_id' => '4', 'permission' => 'content_readable', 'value' => '1'),
 			array('roles_room_id' => '4', 'permission' => 'page_editable', 'value' => '0'),
 			array('roles_room_id' => '4', 'permission' => 'html_not_limited', 'value' => '0'),
-			array('roles_room_id' => '4', 'permission' => 'calendar_creatable', 'value' => '1'),
+			array('roles_room_id' => '4', 'permission' => 'mail_content_receivable ', 'value' => '1'),
 			//--ゲスト
 			array('roles_room_id' => '5', 'permission' => 'block_editable', 'value' => '0'),
 			array('roles_room_id' => '5', 'permission' => 'content_comment_creatable', 'value' => '0'),
@@ -140,7 +140,7 @@ class RoomRecords extends NetCommonsMigration {
 			array('roles_room_id' => '5', 'permission' => 'content_readable', 'value' => '1'),
 			array('roles_room_id' => '5', 'permission' => 'page_editable', 'value' => '0'),
 			array('roles_room_id' => '5', 'permission' => 'html_not_limited', 'value' => '0'),
-			array('roles_room_id' => '5', 'permission' => 'calendar_creatable', 'value' => '0'),
+			array('roles_room_id' => '5', 'permission' => 'mail_content_receivable ', 'value' => '0'),
 			//プライベートスペース
 			//--ルーム管理者
 			array('roles_room_id' => '6', 'permission' => 'block_editable', 'value' => '1'),
@@ -153,7 +153,7 @@ class RoomRecords extends NetCommonsMigration {
 			array('roles_room_id' => '6', 'permission' => 'content_readable', 'value' => '1'),
 			array('roles_room_id' => '6', 'permission' => 'page_editable', 'value' => '1'),
 			array('roles_room_id' => '6', 'permission' => 'html_not_limited', 'value' => '1'),
-			array('roles_room_id' => '6', 'permission' => 'calendar_creatable', 'value' => '1'),
+			array('roles_room_id' => '6', 'permission' => 'mail_content_receivable ', 'value' => '1'),
 			//グループスペース
 			//--ルーム管理者
 			array('roles_room_id' => '7', 'permission' => 'block_editable', 'value' => '1'),
@@ -166,7 +166,7 @@ class RoomRecords extends NetCommonsMigration {
 			array('roles_room_id' => '7', 'permission' => 'content_readable', 'value' => '1'),
 			array('roles_room_id' => '7', 'permission' => 'page_editable', 'value' => '1'),
 			array('roles_room_id' => '7', 'permission' => 'html_not_limited', 'value' => '0'),
-			array('roles_room_id' => '7', 'permission' => 'calendar_creatable', 'value' => '1'),
+			array('roles_room_id' => '7', 'permission' => 'mail_content_receivable ', 'value' => '1'),
 			//--編集長
 			array('roles_room_id' => '8', 'permission' => 'block_editable', 'value' => '1'),
 			array('roles_room_id' => '8', 'permission' => 'content_comment_creatable', 'value' => '1'),
@@ -178,7 +178,7 @@ class RoomRecords extends NetCommonsMigration {
 			array('roles_room_id' => '8', 'permission' => 'content_readable', 'value' => '1'),
 			array('roles_room_id' => '8', 'permission' => 'page_editable', 'value' => '1'),
 			array('roles_room_id' => '8', 'permission' => 'html_not_limited', 'value' => '0'),
-			array('roles_room_id' => '8', 'permission' => 'calendar_creatable', 'value' => '1'),
+			array('roles_room_id' => '8', 'permission' => 'mail_content_receivable ', 'value' => '1'),
 			//--編集者
 			array('roles_room_id' => '9', 'permission' => 'block_editable', 'value' => '0'),
 			array('roles_room_id' => '9', 'permission' => 'content_comment_creatable', 'value' => '1'),
@@ -190,7 +190,7 @@ class RoomRecords extends NetCommonsMigration {
 			array('roles_room_id' => '9', 'permission' => 'content_readable', 'value' => '1'),
 			array('roles_room_id' => '9', 'permission' => 'page_editable', 'value' => '0'),
 			array('roles_room_id' => '9', 'permission' => 'html_not_limited', 'value' => '0'),
-			array('roles_room_id' => '9', 'permission' => 'calendar_creatable', 'value' => '1'),
+			array('roles_room_id' => '9', 'permission' => 'mail_content_receivable ', 'value' => '1'),
 			//--一般
 			array('roles_room_id' => '10', 'permission' => 'block_editable', 'value' => '0'),
 			array('roles_room_id' => '10', 'permission' => 'content_comment_creatable', 'value' => '0'),
@@ -202,7 +202,7 @@ class RoomRecords extends NetCommonsMigration {
 			array('roles_room_id' => '10', 'permission' => 'content_readable', 'value' => '1'),
 			array('roles_room_id' => '10', 'permission' => 'page_editable', 'value' => '0'),
 			array('roles_room_id' => '10', 'permission' => 'html_not_limited', 'value' => '0'),
-			array('roles_room_id' => '10', 'permission' => 'calendar_creatable', 'value' => '1'),
+			array('roles_room_id' => '10', 'permission' => 'mail_content_receivable ', 'value' => '1'),
 			//--ゲスト
 			array('roles_room_id' => '11', 'permission' => 'block_editable', 'value' => '0'),
 			array('roles_room_id' => '11', 'permission' => 'content_comment_creatable', 'value' => '0'),
@@ -214,7 +214,7 @@ class RoomRecords extends NetCommonsMigration {
 			array('roles_room_id' => '11', 'permission' => 'content_readable', 'value' => '1'),
 			array('roles_room_id' => '11', 'permission' => 'page_editable', 'value' => '0'),
 			array('roles_room_id' => '11', 'permission' => 'html_not_limited', 'value' => '0'),
-			array('roles_room_id' => '11', 'permission' => 'calendar_creatable', 'value' => '0'),
+			array('roles_room_id' => '11', 'permission' => 'mail_content_receivable ', 'value' => '0'),
 		),
 		'RoomRole' => array(
 			array('id' => '1', 'role_key' => 'room_administrator', 'level' => '2147483647', 'weight' => '1'),
