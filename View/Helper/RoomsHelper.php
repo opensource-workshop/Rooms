0da31be0e3bfac528a70e0878ab7df1e7f1428ca
s-nakajima <nakajimashouhei@gmail.com>
2016-02-17 18:27:49 +0900

    RoomsHelper::beforeRender()のテスト

--- View/Helper/RoomsHelper.php
+++ View/Helper/RoomsHelper.php
@@ -38,18 +38,16 @@ class RoomsHelper extends AppHelper {
 	);
 
 /**
- * After render file callback.
- * Called after any view fragment is rendered.
+ * Before render callback. beforeRender is called before the view file is rendered.
  *
  * Overridden in subclasses.
  *
- * @param string $viewFile The file just be rendered.
- * @param string $content The content that was rendered.
+ * @param string $viewFile The view file that is going to be rendered
  * @return void
  */
-	public function afterRenderFile($viewFile, $content) {
-		$content = $this->NetCommonsHtml->css('/rooms/css/style.css') . $content;
-		parent::afterRenderFile($viewFile, $content);
+	public function beforeRender($viewFile) {
+		$this->NetCommonsHtml->css('/rooms/css/style.css');
+		parent::beforeRender($viewFile);
 	}
 
 /**
