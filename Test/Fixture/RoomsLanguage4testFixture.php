9beb728327f3981ed6ab4175ce072278b2bf7314
s-nakajima <nakajimashouhei@gmail.com>
2016-02-17 17:48:33 +0900

    RoomsComponent::startup()のテスト

---
+++ Test/Fixture/RoomsLanguage4testFixture.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * RoomsLanguageFixture
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('RoomsLanguageFixture', 'Rooms.Test/Fixture');
+
+/**
+ * RoomsLanguageFixture
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Rooms\Test\Fixture
+ */
+class RoomsLanguage4testFixture extends RoomsLanguageFixture {
+
+/**
+ * Model name
+ *
+ * @var string
+ */
+	public $name = 'RoomsLanguage';
+
+/**
+ * Full Table Name
+ *
+ * @var string
+ */
+	public $table = 'rooms_languages';
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		//パブリックスペース
+		//--日本語
+		array('id' => '1', 'language_id' => '2', 'room_id' => '1', 'name' => 'パブリックスペース'),
+		//--英語
+		array('id' => '2', 'language_id' => '1', 'room_id' => '1', 'name' => 'Public space'),
+		//プライベートスペース
+		//--日本語
+		array('id' => '3', 'language_id' => '2', 'room_id' => '2', 'name' => 'プライベートスペース'),
+		//--英語
+		array('id' => '4', 'language_id' => '1', 'room_id' => '2', 'name' => 'Private space'),
+		//グループスペース
+		//--日本語
+		array('id' => '5', 'language_id' => '2', 'room_id' => '3', 'name' => 'グループスペース'),
+		//--英語
+		array('id' => '6', 'language_id' => '1', 'room_id' => '3', 'name' => 'Group space'),
+	);
+
+}
