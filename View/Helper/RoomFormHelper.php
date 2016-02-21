3b04467f915ef1bdd02ac85ae68088f6f7095855
s-nakajima <nakajimashouhei@gmail.com>
2016-02-21 16:41:47 +0900

    RoomFormHelper::changeStatus()のテスト

--- View/Helper/RoomFormHelper.php
+++ View/Helper/RoomFormHelper.php
@@ -103,6 +103,7 @@ class RoomFormHelper extends AppHelper {
 		$output = '';
 
 		$output .= $this->NetCommonsForm->create('Room', array(
+			'type' => 'put',
 			'url' => $this->NetCommonsHtml->url(array(
 				'action' => 'active', $room['Space']['id'], $room['Room']['id']
 			)),
@@ -110,14 +111,14 @@ class RoomFormHelper extends AppHelper {
 
 		$output .= $this->NetCommonsForm->hidden('Room.id');
 		if ($room['Room']['active']) {
-			$output .= $this->NetCommonsForm->hidden('Room.active', array('value' => false));
+			$output .= $this->NetCommonsForm->hidden('Room.active', array('value' => '0'));
 			$output .= $this->NetCommonsForm->button(__d('rooms', 'It will be in maintenance'), array(
 				'name' => 'save',
 				'class' => 'btn-link',
 				'ng-disabled' => 'sending'
 			));
 		} else {
-			$output .= $this->NetCommonsForm->hidden('Room.active', array('value' => true));
+			$output .= $this->NetCommonsForm->hidden('Room.active', array('value' => '1'));
 			$output .= $this->NetCommonsForm->button(__d('rooms', 'Open the room'), array(
 				'name' => 'save',
 				'class' => 'btn-link',
