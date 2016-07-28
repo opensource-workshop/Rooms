91e0c24745fb867f3bc4407002eab5adefa21045
kteraguchi <kteraguchi@commonsnet.org>
2016-07-28 14:11:55 +0900

    Add ClassRegistry::init('Rooms.Room') befor Form::create

--- Test/Case/View/Helper/RoomsFormHelper/ChangeStatusTest.php
+++ Test/Case/View/Helper/RoomsFormHelper/ChangeStatusTest.php
@@ -49,6 +49,13 @@ class RoomsFormHelperChangeStatusTest extends NetCommonsHelperTestCase {
 
 		//Helperロード
 		$this->loadHelper('Rooms.RoomsForm', $viewVars, $requestData);
+
+		// NetCommonsForm::create('Room', ・・・でapp/Model/Roomが参照される前に、Rooms.Roomをloadさせる。
+		// https://github.com/cakephp/cakephp/blob/7c2d6ae1977c9c135de95177c488b1a8875fc9d6/lib/Cake/View/Helper/FormHelper.php#L171
+		// からの
+		// https://github.com/cakephp/cakephp/blob/97c6850005ed1e50ddcc6a7137138be2c575b654/lib/Cake/Utility/ClassRegistry.php#L144
+		// 通常は、Contorller側で、Rooms.Roomを操作しているはずなので、とりあえずTest実行時のみの対応としとく。
+		ClassRegistry::init('Rooms.Room');
 	}
 
 /**
