2ae58fcdfe40af72aa2f71c5234221aa5bafbce4
s-nakajima <nakajimashouhei@gmail.com>
2017-01-11 18:05:52 +0900

    多言語化対応 https://github.com/NetCommons3/NetCommons3/issues/770

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -206,6 +206,7 @@ class RoomsSchema extends CakeSchema {
 		'room_disk_size' => array('type' => 'biginteger', 'null' => true, 'default' => null, 'unsigned' => false, 'comment' => '各ルームの容量。NULLの場合、無制限。'),
 		'room_id_root' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'permalink' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'is_m17n' => array('type' => 'boolean', 'null' => true, 'default' => '0'),
 		'created_user' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'modified_user' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
